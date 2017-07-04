ac4bc360c1827b6ed6b380e8d473fe7a4530d269
AlienDeg <alien8ali@wp.pl>
2017-07-04 10:43:12 +0200

    Create unnestedSKUs.sql

---
+++ unnestedSKUs.sql
@@ -0,0 +1,10 @@
+with test as (SELECT
+ concat(fullVisitorId, cast(visitId as string)) as sid, 
+ ARRAY_CONCAT_AGG(ARRAY(select productSku from UNNEST(product))) as productSKU, 
+ transaction.transactionId as transactionId
+FROM
+ `dataset_id.ga_sessions_YYYYMMDD3`, UNNEST(hits)
+ where transaction.transactionId is not null
+group by 1,3)
+-- SELECT * from test
+select sid, productSKU, transactionId from test, unnest(productSKU) as productSKU
