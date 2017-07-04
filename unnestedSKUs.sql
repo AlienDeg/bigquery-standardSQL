90b9ff9255300d99df1ab823b4d964d38bf15f39
AlienDeg <alien8ali@wp.pl>
2017-07-04 11:14:06 +0200

    Update unnestedSKUs.sql

--- unnestedSKUs.sql
+++ unnestedSKUs.sql
@@ -1,10 +1,9 @@
-with test as (SELECT
- concat(fullVisitorId, cast(visitId as string)) as sid, 
- ARRAY_CONCAT_AGG(ARRAY(select productSku from UNNEST(product))) as productSKU, 
- transaction.transactionId as transactionId
+with test AS (SELECT
+ concat(fullVisitorId, cast(visitId AS string)) AS sid, 
+ ARRAY_CONCAT_AGG(ARRAY(SELECT productSku FROM UNNEST(product))) AS productSKU, 
+ transaction.transactionId AS transactionId
 FROM
  `dataset_id.ga_sessions_YYYYMMDD`, UNNEST(hits)
- where transaction.transactionId is not null
-group by 1,3)
--- SELECT * from test
-select sid, productSKU, transactionId from test, unnest(productSKU) as productSKU
+ WHERE transaction.transactionId IS NOT null
+GROUP BY 1,3)
+SELECT sid, productSKU, transactionId FROM test, unnest(productSKU) AS productSKU
