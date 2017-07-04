013adfa22f4b5c178a14c4ab35f1546e9ceeac95
AlienDeg <alien8ali@wp.pl>
2017-07-04 11:15:20 +0200

    Update unnestedSKUs.sql

--- unnestedSKUs.sql
+++ unnestedSKUs.sql
@@ -4,6 +4,6 @@ with test AS (SELECT
  transaction.transactionId AS transactionId
 FROM
  `dataset_id.ga_sessions_YYYYMMDD`, UNNEST(hits)
- WHERE transaction.transactionId IS NOT null
+ WHERE transaction.transactionId IS NOT NULL
 GROUP BY 1,3)
 SELECT sid, productSKU, transactionId FROM test, unnest(productSKU) AS productSKU
